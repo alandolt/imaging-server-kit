213c54c9fd37f778adf500261f9f7c3783f57ff2
Mallory Wittwer <mallory.wittwer@epfl.ch>
2024-09-18 16:10:22 +0200

    Add reference deployment (without servers)

---
+++ deployment/README.md
@@ -0,0 +1,26 @@
+![EPFL Center for Imaging logo](https://imaging.epfl.ch/resources/logo-for-gitlab.svg)
+# 🪐 Imaging Server Kit
+
+Start the server:
+
+```
+docker compose up -d
+```
+
+Add a new algorithm:
+
+```
+cookiecutter cookiecutter_template/
+```
+
+## Contributing
+
+Contributions are very welcome.
+
+## License
+
+This project is distributed under the terms of the [BSD-3](http://opensource.org/licenses/BSD-3-Clause) license.
+
+## Issues
+
+If you encounter any problems, please file an issue along with a detailed description.
---
+++ deployment/examples/example_rembg.py
@@ -0,0 +1,32 @@
+"""
+Demo: Running an algorithm.
+"""
+import imaging_server_kit as serverkit
+
+def main():
+    client = serverkit.ServerKitAPIClient()
+    status_code = client.connect("http://localhost:7000")
+    print(f"{status_code=}")
+    print(f"{client.algorithms=}")
+
+    # Get the algo params
+    rembg_params = client.get_algorithm_parameters("rembg")
+    print(f"{rembg_params=}")
+
+    # Get the algo sample image
+    rembg_sample_images = client.get_sample_images("rembg")
+    for image in rembg_sample_images:
+        print(f"{image.shape=}")
+    sample_image = rembg_sample_images[0]
+
+    # Run the algo (return type is a `LayerDataTuple`)
+    data_tuple = client.run_algorithm(
+        algorithm="rembg",
+        image=sample_image,
+        rembg_model_name="silueta"
+    )
+    for (data, data_params, data_type) in data_tuple:
+        print(f"Algo returned: {data_type=} ({data.shape=})")
+
+if __name__ == "__main__":
+    main()
